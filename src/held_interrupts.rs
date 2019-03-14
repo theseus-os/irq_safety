4c4ed4854882180b0ebb0d2710b470b93a0eb6fb
snoword <qiuj@mail.bnu.edu.cn>
2019-03-14 18:20:15 -0500

    add aarch64 support, make x86 stuff depend on target_arch cfg

--- src/held_interrupts.rs
+++ src/held_interrupts.rs
@@ -30,25 +30,42 @@ impl ::core::ops::Drop for HeldInterrupts {
 
 
 // Rust wrappers around the x86-family of interrupt-related instructions.
-
 #[inline(always)]
 pub fn enable_interrupts() {
+    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
     unsafe { asm!("sti" : : : "memory" : "volatile"); }
+
+    #[cfg(any(target_arch="aarch64"))]
+    unsafe { asm!("cpsie i" : : : "memory" : "volatile"); }
+    
 }
 
 
 #[inline(always)]
 pub fn disable_interrupts() {
+    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
     unsafe { asm!("cli" : : : "memory" : "volatile"); }
+
+    #[cfg(any(target_arch="aarch64"))]
+    unsafe { asm!("cpsid i" : : : "memory" : "volatile"); }
 }
 
 
 #[inline(always)]
 pub fn interrupts_enabled() -> bool {
+    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
     unsafe { 
         // we only need the lower 16 bits of the eflags/rflags register
         let flags: usize;
 		asm!("pushf; pop $0" : "=r" (flags) : : "memory" : "volatile");
 		(flags & 0x0200) != 0
      }
+
+    #[cfg(any(target_arch="aarch64"))]
+    unsafe {
+        let primask:usize;  
+        asm!("mrs $0, PRIMASK" : "=r"(primask) : : : "volatile");
+        primask == 0
+    }
+
 }
