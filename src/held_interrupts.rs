809d42eed3e174b1fe3bf143011da928465e3c7f
Klim Tsoutsman <klimusha@gmail.com>
2022-08-24 05:34:39 +1000

    Use compiler instruction fences when enabling/disabling interrupts (#5)
    * For `aarch64` arch, use the proper instructions to control interrupts
    
    * For `arm` arch, remove unnecessary dependency on `cortex-m` crate
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/held_interrupts.rs
+++ src/held_interrupts.rs
@@ -1,84 +1,89 @@
 // Inspired by Tifflin OS
 
-#[cfg(any(target_arch="arm"))]
-extern crate cortex_m;
-
-#[cfg(any(target_arch="arm"))]
-use self::cortex_m::{interrupt, register};
-
-use core::arch::asm;
+use core::{
+    arch::asm,
+    sync::atomic::{compiler_fence, Ordering},
+};
 
 /// A handle for frozen interrupts
 #[derive(Default)]
 pub struct HeldInterrupts(bool);
 
-/// Prevent interrupts from firing until the return value is dropped (goes out of scope). 
-/// After it is dropped, the interrupts are returned to their prior state, not blindly re-enabled. 
+/// Prevent interrupts from firing until the return value is dropped (goes out of scope).
+/// After it is dropped, the interrupts are returned to their prior state, not blindly re-enabled.
 pub fn hold_interrupts() -> HeldInterrupts {
     let enabled = interrupts_enabled();
-	let retval = HeldInterrupts(enabled);
+    let retval = HeldInterrupts(enabled);
     disable_interrupts();
     // trace!("hold_interrupts(): disabled interrupts, were {}", enabled);
     retval
 }
 
-
 impl Drop for HeldInterrupts {
-	fn drop(&mut self) {
+    fn drop(&mut self) {
         // trace!("hold_interrupts(): enabling interrupts? {}", self.0);
-		if self.0 {
-			enable_interrupts();
-		}
-	}
+        if self.0 {
+            enable_interrupts();
+        }
+    }
 }
-
-
-
 
 // Rust wrappers around the x86-family of interrupt-related instructions.
 #[inline(always)]
 pub fn enable_interrupts() {
-    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
-    unsafe { asm!("sti", options(nomem, nostack)); }
+    compiler_fence(Ordering::SeqCst);
+    unsafe {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        asm!("sti", options(nomem, nostack));
 
-    #[cfg(any(target_arch="aarch64"))]
-    unsafe { asm!("cpsie i", options(nomem, nostack)); }
+        #[cfg(target_arch = "aarch64")]
+        // Clear the i and f bits.
+        asm!("msr daifclr, #3", options(nomem, nostack, preserves_flags));
 
-    #[cfg(any(target_arch="arm"))]
-    unsafe { interrupt::enable(); }
+        #[cfg(target_arch = "arm")]
+        asm!("cpsie i", options(nomem, nostack, preserves_flags));
+    }
 }
-
 
 #[inline(always)]
 pub fn disable_interrupts() {
-    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
-    unsafe { asm!("cli", options(nomem, nostack)); }
+    unsafe {
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        asm!("cli", options(nomem, nostack));
 
-    #[cfg(any(target_arch="aarch64"))]
-    unsafe { asm!("cpsid i", options(nomem, nostack)); }
+        #[cfg(target_arch = "aarch64")]
+        // Set the i and f bits.
+        asm!("msr daifset, #3", options(nomem, nostack, preserves_flags));
 
-    #[cfg(any(target_arch="arm"))]
-    interrupt::disable();
+        #[cfg(target_arch = "arm")]
+        asm!("cpsid i", options(nomem, nostack, preserves_flags));
+    }
+    compiler_fence(Ordering::SeqCst);
 }
-
 
 #[inline(always)]
 pub fn interrupts_enabled() -> bool {
-    #[cfg(any(target_arch="x86", target_arch="x86_64"))]
-    unsafe { 
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    unsafe {
         // we only need the lower 16 bits of the eflags/rflags register
         let flags: usize;
         asm!("pushfq; pop {}", out(reg) flags, options(nomem, preserves_flags));
-		(flags & 0x0200) != 0
-     }
-
-    #[cfg(any(target_arch="aarch64"))]
-    unsafe {
-        let primask: usize;  
-        asm!("mrs {}, PRIMASK", out(reg) primask, options(nomem, preserves_flags));
-        primask == 0
+        (flags & 0x0200) != 0
     }
 
-    #[cfg(any(target_arch="arm"))]
-    register::primask::read().is_active()
+    #[cfg(target_arch = "aarch64")]
+    unsafe {
+        let daif: usize;
+        asm!("mrs {}, daif", out(reg) daif, options(nomem, nostack, preserves_flags));
+        // The flags are stored in bits 7-10. We only care about i and f,
+        // stored in bits 7 and 8.
+        daif >> 6 & 0x3 == 0
+    }
+
+    #[cfg(target_arch = "arm")]
+    unsafe {
+        let primask: u32;
+        asm!("mrs {}, primask", out(reg) primask, options(nomem, nostack, preserves_flags));
+        primask & (1 << 0) != (1 << 0)
+    }
 }
